8994118a61324012da32213fd81258185e21b5bd
Rentex1 <43420987+Rentex1@users.noreply.github.com>
2020-04-19 22:30:27 +0200

    being lost and finfining my way

--- Todds Test 2020/Assets/Scripts/myPlayerUI.cs
+++ Todds Test 2020/Assets/Scripts/myPlayerUI.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 public class myPlayerUI : MonoBehaviour
 {
-[Tooltip("UI Text to display Player's Name")]
-[SerializeField]
-private Text playerNameText;
+    [Tooltip("UI Text to display Player's Name")]
+    [SerializeField]
+    private Text playerNameText;
 
-[Tooltip("Pixel offset from the player target")]
-[SerializeField]
-private Vector3 screenOffset = new Vector3(0f,30f,0f);
+    [Tooltip("Pixel offset from the player target")]
+    [SerializeField]
+    private Vector3 screenOffset = new Vector3(0f,30f,0f);
 
-private Movement target;
+    private Movement target;
 
-Transform targetTransform;
-Renderer targetRenderer;
-CanvasGroup _canvasGroup;
-Vector3 targetPosition;
+    Transform targetTransform;
+    Renderer targetRenderer;
+    CanvasGroup _canvasGroup;
+    Vector3 targetPosition;
 
-private void Update() 
-{
-    if (target == null)
+    private void Update() 
     {
-        Destroy(this.gameObject);
-        return;
-    }    
-}
-
-public void SetTarget(Movement _target)
-{
-    if (_target == null)
-    {
-        Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
-        return;
+        if (target == null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }    
     }
-    // Cache references for efficiency
-    target = _target;
-    targetTransform = this.target.GetComponent<Transform>();
-    targetRenderer = this.target.GetComponent<Renderer>();
-    if (playerNameText != null)
+
+    public void SetTarget(Movement _target)
     {
-        playerNameText.text = target.photonView.Owner.NickName;
+        if (_target == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+            return;
+        }
+        // Cache references for efficiency
+        target = _target;
+        targetTransform = this.target.GetComponent<Transform>();
+        targetRenderer = this.target.GetComponent<Renderer>();
+        if (playerNameText != null)
+        {
+            playerNameText.text = target.photonView.Owner.NickName;
+        }
     }
-}
-void Awake()
-{
-    this.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>(), false);
-    _canvasGroup = this.GetComponent<CanvasGroup>();
-}
+    void Awake()
+    {
+        this.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>(), false);
+        _canvasGroup = this.GetComponent<CanvasGroup>();
+    }
 
 }
