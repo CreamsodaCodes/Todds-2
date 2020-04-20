85526a0c3b201ac0fcd3764802657177b88e5566
Rentex1 <43420987+Rentex1@users.noreply.github.com>
2020-04-20 17:27:42 +0200

    Player synced with name

--- Todds Test 2020/Assets/Scripts/myPlayerUI.cs
+++ Todds Test 2020/Assets/Scripts/myPlayerUI.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 public class myPlayerUI : MonoBehaviour
 {
+    void LateUpdate()
+    {
+    // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+        if (targetRenderer!=null)
+        {
+            this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
+        }
+
+
+    // #Critical
+    // Follow the Target GameObject on screen.
+    if (targetTransform != null)
+    {
+        targetPosition = targetTransform.position;
+        targetPosition.y += characterControllerHeight;
+        this.transform.position = Camera.main.WorldToScreenPoint (targetPosition) + screenOffset;
+    }
+    }
     [Tooltip("UI Text to display Player's Name")]
     [SerializeField]
     private Text playerNameText;
 
     [Tooltip("Pixel offset from the player target")]
-    [SerializeField]
-    private Vector3 screenOffset = new Vector3(0f,30f,0f);
+
+    private Vector3 screenOffset = new Vector3(-38f,40f,0f);
 
     private Movement target;
-
+    float characterControllerHeight = 0f;
     Transform targetTransform;
     Renderer targetRenderer;
     CanvasGroup _canvasGroup;
