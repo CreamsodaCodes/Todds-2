d83239115627702fd45f313e3ca8a4281f66f14b
Rentex1 <43420987+Rentex1@users.noreply.github.com>
2020-04-20 16:49:25 +0200

    Name überm und am Player

--- Todds Test 2020/Assets/Scripts/myPlayerUI.cs
+++ Todds Test 2020/Assets/Scripts/myPlayerUI.cs
@@ -4,37 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 public class myPlayerUI : MonoBehaviour
 {
-    [Tooltip("Pixel offset from the player target")]
-    [SerializeField]
-    private Vector3 screenOffset = new Vector3(0f,300f,0f);
-
-    void LateUpdate()
-    {
-        // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
-            if (targetRenderer!=null)
-            {
-                this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
-            }
-
-
-        // #Critical
-        // Follow the Target GameObject on screen.
-        if (targetTransform != null)
-        {
-            targetPosition = targetTransform.position;
-            targetPosition.y += characterControllerHeight;
-            this.transform.position = Camera.main.WorldToScreenPoint (targetPosition) + screenOffset;
-        }
-    }
     [Tooltip("UI Text to display Player's Name")]
     [SerializeField]
     private Text playerNameText;
 
-
-   
+    [Tooltip("Pixel offset from the player target")]
+    [SerializeField]
+    private Vector3 screenOffset = new Vector3(0f,30f,0f);
 
     private Movement target;
-    float characterControllerHeight = 0f;
+
     Transform targetTransform;
     Renderer targetRenderer;
     CanvasGroup _canvasGroup;
@@ -60,12 +39,6 @@ public class myPlayerUI : MonoBehaviour
         target = _target;
         targetTransform = this.target.GetComponent<Transform>();
         targetRenderer = this.target.GetComponent<Renderer>();
-        CharacterController characterController = _target.GetComponent<CharacterController> ();
-        // Get data from the Player that won't change during the lifetime of this Component
-        if (characterController != null)
-        {
-            characterControllerHeight = characterController.height;
-        }
         if (playerNameText != null)
         {
             playerNameText.text = target.photonView.Owner.NickName;
